52d2b055e590d84fd465c03002046fc213a897fa
Yuri Voitovich <voitovich200233@gmail.com>
2022-10-04 21:14:32 +0300

    String generator added

---
+++ string_generator/StringGenerator.cs
@@ -0,0 +1,33 @@
+﻿using generator_interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_generator
+{
+    public class StringGenerator : IGenerator
+    {
+        private Random random = new Random();
+        private const string allowedChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz0123456789!@$?_-";
+
+        public object Generate()
+        {
+            int length = random.Next(6, 20);
+            char[] chars = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = allowedChars[random.Next(0, allowedChars.Length)];
+            }
+
+            return new string(chars);
+        }
+
+        public Type GetGeneratorType()
+        {
+            return typeof(string);
+        }
+    }
+}
